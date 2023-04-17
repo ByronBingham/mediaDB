89e6ce2ca6f91cf9b916aaf59561f2a2d91f1d66
Byron Bingham <bybingham@gmail.com>
2023-04-16 18:09:02 -0600

    added error handling for image comparison function

--- dataIngester/src/main/java/org/bmedia/Utils.java
+++ dataIngester/src/main/java/org/bmedia/Utils.java
@@ -70,45 +70,50 @@ public class Utils {
     }
 
     public static double getImageDiff(String img1_path, String img2_path){
-        BufferedImage img1;
-        BufferedImage img2;
         try {
-            img1 = ImageIO.read(new File(img1_path));
-            img2 = ImageIO.read(new File(img2_path));
-        } catch (IOException e){
-            System.out.println("ERROR: could not open images \"" + img1_path + " and \"" + img2_path + ":\n" + e.getMessage());
-            return -1.0;
-        }
-        int w1 = img1.getWidth();
-        int w2 = img2.getWidth();
-        int h1 = img1.getHeight();
-        int h2 = img2.getHeight();
-        if ((w1!=w2)||(h1!=h2)) {
-            System.out.println("Both images should have same dimensions");
-            return 100.0;
-        } else {
-            long diff = 0;
-            for (int j = 0; j < h1; j++) {
-                for (int i = 0; i < w1; i++) {
-                    //Getting the RGB values of a pixel
-                    int pixel1 = img1.getRGB(i, j);
-                    Color color1 = new Color(pixel1, true);
-                    int r1 = color1.getRed();
-                    int g1 = color1.getGreen();
-                    int b1 = color1.getBlue();
-                    int pixel2 = img2.getRGB(i, j);
-                    Color color2 = new Color(pixel2, true);
-                    int r2 = color2.getRed();
-                    int g2 = color2.getGreen();
-                    int b2= color2.getBlue();
-                    //sum of differences of RGB values of the two images
-                    long data = Math.abs(r1-r2)+Math.abs(g1-g2)+ Math.abs(b1-b2);
-                    diff = diff+data;
-                }
+            BufferedImage img1;
+            BufferedImage img2;
+            try {
+                img1 = ImageIO.read(new File(img1_path));
+                img2 = ImageIO.read(new File(img2_path));
+            } catch (IOException e) {
+                System.out.println("ERROR: could not open images \"" + img1_path + " and \"" + img2_path + ":\n" + e.getMessage());
+                return -1.0;
             }
-            double avg = diff/(w1*h1*3);
-            double percentage = (avg/255)*100;
-            return percentage;
+            int w1 = img1.getWidth();
+            int w2 = img2.getWidth();
+            int h1 = img1.getHeight();
+            int h2 = img2.getHeight();
+            if ((w1 != w2) || (h1 != h2)) {
+                System.out.println("Both images should have same dimensions");
+                return 100.0;
+            } else {
+                long diff = 0;
+                for (int j = 0; j < h1; j++) {
+                    for (int i = 0; i < w1; i++) {
+                        //Getting the RGB values of a pixel
+                        int pixel1 = img1.getRGB(i, j);
+                        Color color1 = new Color(pixel1, true);
+                        int r1 = color1.getRed();
+                        int g1 = color1.getGreen();
+                        int b1 = color1.getBlue();
+                        int pixel2 = img2.getRGB(i, j);
+                        Color color2 = new Color(pixel2, true);
+                        int r2 = color2.getRed();
+                        int g2 = color2.getGreen();
+                        int b2 = color2.getBlue();
+                        //sum of differences of RGB values of the two images
+                        long data = Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+                        diff = diff + data;
+                    }
+                }
+                double avg = diff / (w1 * h1 * 3);
+                double percentage = (avg / 255) * 100;
+                return percentage;
+            }
+        }catch (Exception e){
+            System.out.println("WARNING: Problem getting image diff for \"" + img1_path + " and \"" + img2_path + ":\n" + e.getMessage());
+            return 100.0;
         }
     }
 
